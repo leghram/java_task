ef93d7a45f80a6c77bdec4b2d8b8b74f59ab1eb8
Miguel Paricahua Lopez <mig.parlop@gmail.com>
2023-07-09 20:21:29 -0500

    bd conection working

---
+++ src/main/utils/DatabaseConnector.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnector {
+    private static final String URL = "jdbc:mysql://localhost:3306/java?useSSL=false";
+    private static final String USERNAME = "myuser";
+    private static final String PASSWORD = "mysecretpassword";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Error al cargar el controlador JDBC", e);
+        }
+    }
+}
